1d46056d2fd22a627f6205093fdfb5348c15f937
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-12-16 21:05:57 -0300

    add background color

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const Container = styled.div`
   background: rgb(0, 11, 73);
   background: linear-gradient(
     315deg,
-    rgb(0 11 73) 0%,
-    rgb(4 21 98 / 86%) 68%,
-    rgba(38, 102, 207, 0.9654061453683036) 100%
+    rgb(0 11 73 / 73%) 0%,
+    rgb(4 21 98 / 72%) 68%,
+    rgb(38 102 207 / 87%) 100%
   );
   width: 100vw;
   font-family: 'Ubuntu', sans-serif;
